7a5eb6e9a4ea3fb8ebf9b1fcc7c059ff6ed52b09
devsakibx <devsakibx@gmail.com>
2025-02-04 01:08:17 +0600

    Final commit. No more changes. I swear... 🙃

--- tailwind.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    content: ["./index.html"], // Files where Tailwind should look for classes
-    theme: {
-      extend: {},
-    },
-    plugins: [require("daisyui")], // Adding DaisyUI plugin
-  }
